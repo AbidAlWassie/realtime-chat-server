c99dcc47c8a728d061788d142c8e2b3af4935d06
Abid Al Wassie <abidalwassie@outlook.com>
2024-11-02 10:43:56 +0600

    added dist dir

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+// server/index.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+    socket.on("join_room", (room) => {
+        socket.join(room);
+        console.log(`User with ID: ${socket.id} joined room: ${room}`);
+    });
+    socket.on("send_message", (data) => {
+        const messageData = Object.assign(Object.assign({}, data), { senderId: socket.id });
+        io.to(data.room).emit("receive_message", messageData);
+    });
+});
+server.listen(3001, () => {
+    console.log("SERVER IS RUNNING on port 3001");
+});
